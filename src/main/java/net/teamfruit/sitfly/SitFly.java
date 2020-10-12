a3f59037e79cabb734ed96d977f418f94f3e84b1
Kamesuta <kamesuta@gmail.com>
2020-10-12 18:22:47 +0900

    Speed

--- src/main/java/net/teamfruit/sitfly/SitFly.java
+++ src/main/java/net/teamfruit/sitfly/SitFly.java
@@ -38,6 +38,7 @@ public final class SitFly extends JavaPlugin implements Listener {
         // Plugin startup logic
         logger = getLogger();
         horseKey = new NamespacedKey(this, "fly_horse");
+        saveDefaultConfig();
         getServer().getPluginManager().registerEvents(this, this);
     }
 
@@ -111,7 +112,7 @@ public final class SitFly extends JavaPlugin implements Listener {
         horse.getInventory().setSaddle(new ItemStack(Material.SADDLE));
         horse.setGravity(false);
         horse.setJumpStrength(5);
-        horse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(5);
+        horse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(getConfig().getDouble("settings.speed"));
 
         // タグ付け
         horse.getPersistentDataContainer().set(horseKey, PersistentDataType.BYTE, (byte) 1);
@@ -135,7 +136,7 @@ public final class SitFly extends JavaPlugin implements Listener {
         Location loc = entity.getLocation();
         if (loc.getBlock().getRelative(BlockFace.DOWN, 2).isEmpty()) {
             event.setCancelled(true);
-            entity.setVelocity(new Vector(0, -.5, 0));
+            entity.setVelocity(new Vector(0, -getConfig().getDouble("settings.down"), 0));
         } else {
             entity.remove();
         }
@@ -153,8 +154,7 @@ public final class SitFly extends JavaPlugin implements Listener {
         // Spaceしたら少し上がる
         Location loc = entity.getLocation();
         if (loc.getBlock().getRelative(BlockFace.UP, 2).isEmpty()) {
-            logger.info("" + event.getPower());
-            entity.setVelocity(new Vector(0, event.getPower() * event.getPower() * 2, 0));
+            entity.setVelocity(new Vector(0, event.getPower() * event.getPower() * getConfig().getDouble("settings.jump"), 0));
         }
     }
 }
